27a678752c0d45c2beda34d7571330237e7977b2
Douglas Lopes <douglasdmlopes@gmail.com>
2021-06-03 12:26:09 -0300

    Estilizando barra de rolagem

--- src/pages/main/index.vue
+++ src/pages/main/index.vue
@@ -8,7 +8,29 @@
       <h3 class="text-content"> Escolha a categoria desejada</h3>
     </div>
 
+    <div class="categories-div">
+      <ul class="categories">
+        <li class="category-item">
+          <img src="@/assets/images/rocket.svg" class="icon"/>
+          <p>Item</p>
+        </li>
+        <li class="category-item">
+          <img src="@/assets/images/rocket.svg" class="icon"/>
+          <p>Item</p>
+        </li>
+        <li class="category-item">
+          <img src="@/assets/images/rocket.svg" class="icon"/>
+          <p>Item</p>
+        </li>
+        <li class="category-item">
+          <img src="@/assets/images/rocket.svg" class="icon"/>
+          <p>Item</p>
+        </li>
+      </ul>
+    </div>
+
   </div>
+  
 </template>
 
 <script>
@@ -32,13 +54,30 @@ export default ({
   width: 242px;
 }
 
+.icon {
+  width: 1.5rem;
+}
+
+.categories-div {
+  margin: 0px 1.875rem;
+}
+
+.category-item {
+  display: grid;
+  grid-template-columns: 1fr 6fr;
+  grid-gap: 15px;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  align-content: center;
+}
+
 .image-part {
   display: grid;
   justify-content: center;
 }
 
 .text-part {
-  padding: 1.1rem 1.875rem 2.125rem;
+  padding: 1.1rem 1.875rem 1.125rem;
 }
 
 .text-title {
